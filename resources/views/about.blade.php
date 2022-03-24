3ccb8a97a50ff9c7bcfcc77f04666479223ae3ac
wiwin.ristanto <wiwinristanto94@gmail.com>
2022-03-24 13:12:37 +0700

    update

---
+++ resources/views/about.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Latihan Laravel 1</title>
+</head>
+<body>
+    <h1>Halaman About</h1>
+    <h1>Wiwin Ristanto</h1>
+    <p>wiwinristanto@gmail.com</p>
+    <img src="img/img_1.jpg" alt="Wiwin R" width="200px">
+</body>
+</html>
